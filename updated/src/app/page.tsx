7c2ba4386f34dddd374a8f51d02a8a44fea05a2b
adeshingale3 <adeshingale600@gmail.com>
2025-10-10 13:46:28 +0530

    projects

--- updated/src/app/page.tsx
+++ updated/src/app/page.tsx
@@ -18,13 +18,13 @@ const page = () => {
         <h3 className='text-wrap ml-3 text-[13px] w-[70%] text-white/70'>Software Engineer from India. I love to develope sexy frontend designs more than my non-existing girlfriend.</h3>
         <Skills />
         <Socials />
-        <h3 className='mt-6 ml-3'>Experience --------------------------------------</h3>
+        <h3 className='mt-6 ml-3 text-[20px] font-bold'>Experience </h3>
         <Experience />
-        <h3 className='mt-6 ml-3'>Projects --------------------------------------</h3>
+        <h3 className='mt-6 ml-3 text-[20px] font-bold'>Projects </h3>
         <Projects />
-        <h3 className='mt-6 ml-3'>Education --------------------------------------</h3>
+        <h3 className='mt-6 ml-3 text-[20px] font-bold'>Education</h3>
         <Education />
-        <div className='h-20 w-full mt-15 flex justify-center items-center'>
+        <div className='h-20 w-full mt-15 flex justify-center items-center text-center mr-3'>
           <h3 className='mb-10 italic text-[15px] text-gray-400'>Liked my work? Want to collaborate? Feel free to reach out to me on <a href='https://x.com/adesshhhhh' className='font-bold text-white' >X</a>  or <a href='mailto:adeshingale600@gmail.com' className='font-bold text-white' >Email</a></h3>
         </div>
       </div>
